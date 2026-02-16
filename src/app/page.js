26ac582972e910325d45cd77d5e43a5d7ff97a75
Hyper-RP <rp302084@gmail.com>
2026-02-16 17:18:12 +0530

    final changes for orientation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
 import { cookies } from "next/headers";
 
-
 export default async function Home() {
-  
   return (
-    <div className="">
-      Nothing....
+    <div className="flex justify-center items-center text-lg mt-6">
+      Select a device to get started...
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import { useContext, useState, useEffect } from "react";
 import { FaMoon } from "react-icons/fa";
@@ -12,55 +13,67 @@ function Navbar() {
   const [theme, setTheme] = useState("dark");
   const params = useParams();
   const router = useRouter();
-  const { fetchData,setData} = useContext(apiContext);
+  const { fetchData, setData } = useContext(apiContext);
   async function viewHandler() {
     router.push(params.device === "mobile" ? "/laptop" : "/mobile");
   }
 
   function themeHandler() {
-    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
-    document.cookie = `theme=${theme};path=/`;
+    const newTheme=theme==="dark" ? "light" : "dark";
+    setTheme(newTheme);
+    document.cookie = `theme=${newTheme};path=/`;
     router.refresh();
   }
+
   useEffect(() => {
-    setData([])
+    document.cookie = `theme=${theme};path=/`;
+  },[]);
+
+  useEffect(() => {
+    setData([]);
     fetchData();
   }, [params.device]);
 
   return (
     <div className="w-10/12 relative mx-auto flex justify-center ">
-      <Image
-        className="transition-colors duration-1000 ease-in-out"
-        src={`${theme === "dark" ? "/logo/R-img.jpg" : "/logo/R-logo2.png"}`}
-        width={100}
-        height={100}
-        alt="not found"
-      ></Image>
+      <motion.div
+  key={theme}
+  initial={{ opacity: 0 }}
+  animate={{ opacity: 1 }}
+  transition={{ duration: 0.5 }}
+>
+  <Image
+    src={theme === "light" ? "/logo/R-img.jpg" : "/logo/R-logo2.png"}
+    width={100}
+    height={100}
+    alt="logo"
+  />
+</motion.div>
       <div className="flex w-[5rem] mt-[1rem]  absolute right-[2rem] justify-between">
         {theme === "dark" ? (
           <FaMoon
             size={23}
             onClick={() => themeHandler()}
-            className="mt-[0.3rem] text-[black]"
+            className="mt-[0.3rem] text-[white]"
           />
         ) : (
           <MdSunny
             size={23}
             onClick={() => themeHandler()}
-            className="mt-[0.3rem] text-[white]"
+            className="mt-[0.3rem] text-[black]"
           />
         )}
         {params.device === "laptop" ? (
           <PiDeviceMobileSpeakerLight
             onClick={() => viewHandler()}
             size={30}
-            className={`${theme === "dark" ? "text-[black]" : "text-[white]"}`}
+            className={`${theme === "light" ? "text-[black]" : "text-[white]"}`}
           />
         ) : (
           <CiLaptop
             onClick={() => viewHandler()}
             size={33}
-            className={`${theme === "dark" ? "text-[black]" : "text-[white]"}`}
+            className={`${theme === "light" ? "text-[black]" : "text-[white]"}`}
           />
         )}
       </div>
